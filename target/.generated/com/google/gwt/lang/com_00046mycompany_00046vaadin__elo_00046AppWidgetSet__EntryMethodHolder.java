e24d61782f800f66dad51f3664a1f6d1d32c1be3
bwinslawski <wisla162@gmail.com>
2014-05-26 20:30:59 +0200

    Dodanie projektu

---
+++ target/.generated/com/google/gwt/lang/com_00046mycompany_00046vaadin__elo_00046AppWidgetSet__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046mycompany_00046vaadin__elo_00046AppWidgetSet__EntryMethodHolder {
+  public static final void init() {
+    // Filled in by the compiler to call entry methods.
+  }
+}
